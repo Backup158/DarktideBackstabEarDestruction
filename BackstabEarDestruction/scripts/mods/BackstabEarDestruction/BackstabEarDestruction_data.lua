b61df91b30c48529f7fea54cf6a03e21e9e945cc
Backup158 <beekoop158@gmail.com>
2025-05-16 04:58:15 -0500

    refactor: condense nonaudio replacements to a loop for easier modification (same replacement options for each dropdown)

--- BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_data.lua
+++ BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_data.lua
@@ -1,4 +1,32 @@
 local mod = get_mod("BackstabEarDestruction")
+
+local backstab_options = {"melee", "melee_elite", "ranged",}
+local final_nonaudio_widgets = {}
+
+--local widgets_location = final_data_table.options.widgets
+--local nonaudio_subwidgets = widgets_location[#widgets_location].sub_widgets
+for _, backstab_event in pairs(backstab_options) do
+    -- insert at the tail (index of size plus 1)
+    final_nonaudio_widgets[#final_nonaudio_widgets + 1] = {
+        setting_id = "replace_nonaudio_"..backstab_event,
+        type = "checkbox",
+        default_value = false,
+        sub_widgets = {
+            {
+                setting_id = "replace_nonaudio_"..backstab_event.."_choice",
+                type = "dropdown",
+                default_value = "havoc_terminal_rank_up_final_tier",
+                options = {
+                    {text = backstab_event.."_option_havoc_final", value = "havoc_terminal_rank_up_final_tier"},
+                    {text = backstab_event.."_option_crafting_trait", value = "crafting_view_on_replace_trait"},
+                    {text = backstab_event.."_option_crafting_upgrade", value = "crafting_view_on_upgrade_item"},
+                    {text = backstab_event.."_option_crafting_sacrifice", value = "crafting_view_sacrifice_weapon"},
+                }
+            },
+        }
+    }
+end
+
 return {
     name = mod:localize("mod_name"),
     description = mod:localize("mod_description"),
@@ -36,62 +64,8 @@ return {
 				setting_id = "nonaudio_replacements",
 				type = "group",
 				default_value = false,
-                sub_widgets = {
-                    {
-                        setting_id = "replace_nonaudio_melee",
-                        type = "checkbox",
-                        default_value = false,
-                        sub_widgets = {
-                            {
-                                setting_id = "replace_nonaudio_melee_choice",
-                                type = "dropdown",
-                                default_value = "havoc_terminal_rank_up_final_tier",
-                                options = {
-                                    {text = "melee_option_havoc_final", value = "havoc_terminal_rank_up_final_tier"},
-                                    {text = "melee_option_crafting_upgrade", value = "crafting_view_on_upgrade_item"},
-                                    {text = "melee_option_crafting_sacrifice", value = "crafting_view_sacrifice_weapon"},
-                                    {text = "melee_option_crafting_extract", value = "crafting_view_on_extract_trait"},
-                                }
-                            },
-                        }
-                    },
-                    {
-                        setting_id = "replace_nonaudio_melee_elite",
-                        type = "checkbox",
-                        default_value = false,
-                        sub_widgets = {
-                            {
-                                setting_id = "replace_nonaudio_melee_elite_choice",
-                                type = "dropdown",
-                                default_value = "havoc_terminal_rank_up_final_tier",
-                                options = {
-                                    {text = "melee_elite_option_havoc_final", value = "havoc_terminal_rank_up_final_tier"},
-                                    {text = "melee_elite_option_crafting_upgrade", value = "crafting_view_on_upgrade_item"},
-                                    {text = "melee_elite_option_crafting_sacrifice", value = "crafting_view_sacrifice_weapon"},
-                                    {text = "melee_elite_option_crafting_extract", value = "crafting_view_on_extract_trait"},
-                                }
-                            },
-                        }
-                    },
-                    {
-                        setting_id = "replace_nonaudio_ranged",
-                        type = "checkbox",
-                        default_value = false,
-                        sub_widgets = {
-                            {
-                                setting_id = "replace_nonaudio_ranged_choice",
-                                type = "dropdown",
-                                default_value = "havoc_terminal_rank_up_final_tier",
-                                options = {
-                                    {text = "ranged_option_havoc_final", value = "havoc_terminal_rank_up_final_tier"},
-                                    {text = "ranged_option_crafting_upgrade", value = "crafting_view_on_upgrade_item"},
-                                    {text = "ranged_option_crafting_sacrifice", value = "crafting_view_sacrifice_weapon"},
-                                    {text = "ranged_option_crafting_extract", value = "crafting_view_on_extract_trait"},
-                                }
-                            },
-                        }
-                    },
-                }
+                -- To be filled by loop
+                sub_widgets = final_nonaudio_widgets,
 			},
         }
 	}
