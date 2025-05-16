b92328e1305a591f25e3b2f3f2623caf5ca554c0
Backup158 <beekoop158@gmail.com>
2025-05-16 05:11:20 -0500

    feat: localized mod

--- BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_localization.lua
+++ BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_localization.lua
@@ -3,6 +3,24 @@ return {
         en = "Backstab Ear Destruction",
     },
     mod_description = {
-        en = "MAKES BACKSTAB SOUNDS VERY LOUD using the audio plugin",
+        en = "REPLACES BACKSTAB SOUNDS WITH SOMETHING LOUD using the Audio plugin",
+    },
+    enable_debug_mode = {
+        en = "Debug Mode",
+    },
+    enable_debug_mode_description = {
+        en = "Enables verbose logging (does nothing atm)",
+    },
+    use_audio = {
+        en = "Backstab Events to Replace",
+    },
+    replace_indicator_melee = {
+        en = "Melee",
+    },
+    replace_indicator_melee_elite = {
+        en = "Melee Elites",
+    },
+    replace_indicator_ranged = {
+        en = "Ranged",
     },
 }
