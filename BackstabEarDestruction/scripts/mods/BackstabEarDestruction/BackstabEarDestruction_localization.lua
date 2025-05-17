9c974c897395189c517aba8d772d5331e9055ffa
Backup158 <beekoop158@gmail.com>
2025-05-17 00:02:16 -0500

    fix: sentence case to also check just first letter, for single word strings

--- BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_localization.lua
+++ BackstabEarDestruction/scripts/mods/BackstabEarDestruction/BackstabEarDestruction_localization.lua
@@ -18,27 +18,31 @@ local localizations = {
 
 local backstab_events = {"melee", "melee_elite", "ranged", }
 
-local function to_sentence_case(str)
-    return (str:gsub("\b%l", string.upper))
-end
-
 local function add_localization_format(event_name, base_key, base_localization, will_append)
-    local final_localization_desc
+    local final_localization_val
     if will_append then 
-        final_localization_desc = base_localization..event_name
+        final_localization_val = base_localization..event_name
     else
-        final_localization_desc = event_name..base_localization
+        final_localization_val = event_name..base_localization
     end
-    final_localization_desc = to_sentence_case(final_localization_desc)
+    -- Replaces underscores with spaces
+    final_localization_val = string.gsub(final_localization_val, "_", " ")
+    -- Converts string value to sentence case
+    --  \b is word boundary
+    --  %l is lowercase letter
+    --  ^ is start of string
+    final_localization_val = string.gsub(final_localization_val, "^%l", string.upper)
+    final_localization_val = string.gsub(final_localization_val, " %l", string.upper)
 
     localizations[base_key..event_name] = {
-        en = final_localization_desc
+        en = final_localization_val
     }
 
 end
 
 for _, event_name in pairs(backstab_events) do
-    add_localization_format(event_name, "replace_indicator", "", true)
+    add_localization_format(event_name, "replace_indicator_", "", true)
+    add_localization_format(event_name, "replacement_sound_volume_", "Volume for ", true)
 end
 
 return localizations
